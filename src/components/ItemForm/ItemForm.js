546a13b3073d1a3a193ee332356d52cf90218ce5
Satu Satomaa <satu.satomaa@edu.sasky.fi>
2020-04-26 14:45:13 +0300

    Stats graphics added

--- src/components/ItemForm/ItemForm.js
+++ src/components/ItemForm/ItemForm.js
@@ -79,13 +79,13 @@ class ItemForm extends React.Component {
             <div>
                 <label htmlFor="paivat">Tavoite</label>
                 <select name="paivat" value={this.state.data.paivat} onChange={this.handleInputChange}>
-                    <option value="0">1</option>
-                    <option value="1">2</option>
-                    <option value="2">3</option>
-                    <option value="3">4</option>
-                    <option value="4">5</option>
-                    <option value="5">6</option>
-                    <option value="6">7</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
                 </select>
                 </div>
             </div>
--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -2,10 +2,47 @@ import React from 'react';
 
 import Content from '..//Content/Content';
 
+import { Doughnut } from 'react-chartjs-2';
+
+import './Stats.css';
+
 function Stats(props) {
+
+  const reducer = (groupedData, currentItem) => {
+    const index = groupedData.findIndex(item => item.tyyppi === currentItem.tyyppi);
+    if (index >= 0) {
+      groupedData[index].toteutunut = groupedData[index].toteutunut + currentItem.toteutunut;
+    } else {
+      groupedData.push({tyyppi: currentItem.tyyppi, toteutunut: currentItem.toteutunut});
+    }
+    return groupedData;
+  }
+
+  let groupedData = props.data.reduce(reducer, []);
+
+  let doughnutData = {
+    labels: groupedData.map(item => item.tyyppi),
+    datasets: [
+      {
+      data: groupedData.map(item => item.toteutunut),
+      backgroundColor: [
+        '#FF6384',
+        '#36A2EB',
+        '#FFCE56'
+        ]
+      }
+    ]    
+  }
+
     return (
       <Content>
-        <h2>Stats</h2>
+        <div className="stats">
+          <h2>Tehtävätilasto</h2>
+          <h3>Tehtävätyypit</h3>
+          <div className="stats__graph">
+            <Doughnut data={doughnutData} />
+        </div>
+      </div>
       </Content>
     );
   }
